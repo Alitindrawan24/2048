b4094c8770c129bcf4a7a168003658bd839904f1
harshu0001 <prataph229@gmail.com>
2023-10-10 01:10:43 +0530

    Buttons added

--- app.js
+++ app.js
@@ -312,3 +312,19 @@ function onClickBtnHowTo() {
 function closeHowToDialog() {
     dialogHowTo.close();
 }
+
+//Theme Switcher
+var body = document.querySelector("body"), // Creates a variable called body, so we can reference it more easily below.
+  goLight = function() { // Creates a function called goLight that adds the 'dark' class to the body
+    body.className = "";
+  },
+  goDark = function() { // Creates a function called goDark that removes the 'dark' class from the body
+    body.className = "";
+    body.classList.add("dark");
+  }
+
+document.querySelector(".theme1").addEventListener("click", goLight, false); // Tells the first button to run the goLight function when clicked
+
+document.querySelector(".theme2").addEventListener("click", goDark, false); // Tells the second button to run the goDark function when clicked
+
+switchImmediately(); // Invoke the function
