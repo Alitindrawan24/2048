c0b0d2422ede2a28a7a4a4a8696a46e9cff52321
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-10-30 19:10:26 +0545

    fix: style issue and code placement

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then((registration) =>
+      console.log(`Service Worker registered with scope: ${registration.scope}`)
+    )
+    .catch((error) =>
+      console.log(`Service Worker registration failed:
+        ${error}`)
+    );
+}
+
 let dialogHowTo = document.querySelector("#dialog-how-to");
 let stopGame = false;
 
